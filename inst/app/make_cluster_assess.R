31de965d4c89802130af14e77fd70e39935f2ffd
David McGaughey <mcgaughey@gmail.com>
2021-11-12 21:27:19 -0500

    functionalize this section

--- inst/app/make_cluster_assess.R
+++ inst/app/make_cluster_assess.R
@@ -5,28 +5,28 @@
 #                                       is.na(CellType_predict) ~ 'Unlabelled',
 #                                       TRUE ~ CellType_predict))
 
-meta_filter_TM <- data.table::fread('~/data/scEiaD_v2/metadata_filter.tsv.gz') %>%
+meta_filter_TM <- fst::read_fst('~/data/scEiaD_v3/2021_11_09_meta_filter.fst') %>%
   mutate(CellType_predict = case_when(!is.na(TabulaMurisCellType_predict) ~ 'Tabula Muris',
                                       is.na(CellType_predict) ~ 'Unlabelled',
-                                      TRUE ~ CellType_predict)) %>%
-  mutate(CellType_predict = case_when(TabulaMurisCellType_predict == 'T cell' ~ 'T/NK-Cell',
-                                      TabulaMurisCellType_predict == 'B cell' ~ 'B-Cell',
-                                      TabulaMurisCellType_predict == 'endothelial cell' ~ 'Endothelial',
-                                      TabulaMurisCellType_predict == 'epithelial cell' ~ 'Epithelial',
-                                      TabulaMurisCellType_predict == 'endothelial cell' ~ 'Epithelial',
-                                      TabulaMurisCellType_predict == 'keratinocyte' ~ 'Keratinocyte',
-                                      TabulaMurisCellType_predict == 'blood cell' ~ 'Red Blood Cell',
-                                      TabulaMurisCellType_predict == 'hepatocyte' ~ 'Hepatocyte',
-                                      TabulaMurisCellType_predict == 'mesenchymal cell' ~ 'Mesenchymal',
-                                      TabulaMurisCellType_predict == 'bladder cell' ~ 'Bladder',
-                                      TabulaMurisCellType_predict == 'mesenchymal stem cell' ~ 'Mesenchymal (Stem)',
-                                      TabulaMurisCellType_predict == 'bladder urothelial cell' ~ 'Bladder Urothelial',
-                                      TabulaMurisCellType_predict == 'kidney proximal straight tubule epithelial cell' ~ 'Kidney Proximal Tubule',
-                                      TabulaMurisCellType_predict == 'basal cell of epidermis' ~ 'Basal Cell',
-                                      TabulaMurisCellType_predict == 'macrophage' ~ 'Macrophage',
-                                      TabulaMurisCellType_predict == 'natural killer cell' ~ 'T/NK-Cell',
-                                      TabulaMurisCellType_predict == 'monocyte' ~ 'Monocyte',
-                                      TRUE ~ CellType_predict))
+                                      TRUE ~ CellType_predict)) #%>%
+  # mutate(CellType_predict = case_when(TabulaMurisCellType_predict == 'T cell' ~ 'T/NK-Cell',
+  #                                     TabulaMurisCellType_predict == 'B cell' ~ 'B-Cell',
+  #                                     TabulaMurisCellType_predict == 'endothelial cell' ~ 'Endothelial',
+  #                                     TabulaMurisCellType_predict == 'epithelial cell' ~ 'Epithelial',
+  #                                     TabulaMurisCellType_predict == 'endothelial cell' ~ 'Epithelial',
+  #                                     TabulaMurisCellType_predict == 'keratinocyte' ~ 'Keratinocyte',
+  #                                     TabulaMurisCellType_predict == 'blood cell' ~ 'Red Blood Cell',
+  #                                     TabulaMurisCellType_predict == 'hepatocyte' ~ 'Hepatocyte',
+  #                                     TabulaMurisCellType_predict == 'mesenchymal cell' ~ 'Mesenchymal',
+  #                                     TabulaMurisCellType_predict == 'bladder cell' ~ 'Bladder',
+  #                                     TabulaMurisCellType_predict == 'mesenchymal stem cell' ~ 'Mesenchymal (Stem)',
+  #                                     TabulaMurisCellType_predict == 'bladder urothelial cell' ~ 'Bladder Urothelial',
+  #                                     TabulaMurisCellType_predict == 'kidney proximal straight tubule epithelial cell' ~ 'Kidney Proximal Tubule',
+  #                                     TabulaMurisCellType_predict == 'basal cell of epidermis' ~ 'Basal Cell',
+  #                                     TabulaMurisCellType_predict == 'macrophage' ~ 'Macrophage',
+  #                                     TabulaMurisCellType_predict == 'natural killer cell' ~ 'T/NK-Cell',
+  #                                     TabulaMurisCellType_predict == 'monocyte' ~ 'Monocyte',
+  #                                     TRUE ~ CellType_predict))
 
 meta_filter_TM <- meta_filter_TM %>% mutate(Age = as.character(Age),
                                             cluster = as.character(cluster),
@@ -50,78 +50,61 @@ cat_to_color_df <- lapply(categorical_columns, function(col) map_color(col, meta
 
 
 
-## CellType_predict
-pdata <- meta_filter_TM %>% ungroup()%>% group_by(CellType_predict,  cluster) %>%
-  summarise(Count = n()) %>%
-  left_join(cat_to_color_df %>%
-              filter(meta_category == 'CellType_predict') %>%
-              select(value, color) %>% unique(),
-            by = c('CellType_predict' = 'value')) %>%
-  ungroup()
-col <- pdata$color
-names(col) <- pdata$CellType_predict
-cluster_plot <- pdata %>%
-  mutate(cluster = factor(cluster, levels = seq(1,max(pdata$cluster %>% as.numeric()),1) %>% as.character())) %>%
-  ggplot(aes(x=cluster, y= Count, fill = CellType_predict)) +
-  geom_bar(stat = 'identity') + cowplot::theme_cowplot() +
-  scale_y_continuous(expand = c(0,0)) +
-  coord_flip() +
-  scale_fill_manual(values = col) +
-  guides(fill=guide_legend(ncol=1))
+cluster_facet_maker <- function(meta_filter,
+                                faceting_on = 'CellType_predict',
+                                filter_on = NA,
+                                perc_cutoff = 10){
+  pdata <- meta_filter %>%
+    ungroup() %>%
+    group_by(!!as.symbol(faceting_on),  cluster) %>%
+    summarise(Count = n())
+  colnames(pdata)[1] <- 'value'
+  pdata <- pdata %>%
+    left_join(cat_to_color_df %>%
+                filter(meta_category == faceting_on) %>%
+                select(value, color) %>% unique(),
+              by =  'value') %>%
+    ungroup()
+  col <- pdata$color
+  names(col) <- pdata$value
 
-## cluster facet
-facet_plot <- pdata %>%
-  group_by(cluster) %>%
-  mutate(Percentage = (Count / sum(Count) * 100)) %>%
-  filter(Percentage > 10)  %>%
-  mutate(cluster = factor(cluster, levels = seq(1,max(pdata$cluster %>% as.numeric()),1) %>% as.character())) %>%
-  ggplot(aes(x=CellType_predict, y=Count, fill = CellType_predict)) +
-  geom_bar(stat = 'identity') + cowplot::theme_cowplot() +
-  scale_y_continuous(expand = c(0,0)) +
-  coord_flip() +
-  scale_fill_manual(values = col) +
-  facet_wrap(~cluster, scales = 'free', ncol = 5) +
-  theme(legend.position = "none")
+  if (!is.na(filter_on)){
+    pdata <- pdata %>% filter(cluster == filter_on)
+  }
 
-## phase
-pdata <- meta_filter_TM %>% ungroup()%>% group_by(Phase,  cluster) %>%
-  summarise(Count = n()) %>%
-  left_join(cat_to_color_df %>%
-              filter(meta_category == 'Phase') %>%
-              select(value, color) %>% unique(),
-            by = c('Phase' = 'value')) %>%
-  ungroup()
-col <- pdata$color
-names(col) <- pdata$Phase
-phase_plot <- pdata %>%
-  mutate(cluster = factor(cluster, levels = seq(1,max(pdata$cluster %>% as.numeric()),1) %>% as.character())) %>%
-  ggplot(aes(x=cluster, y= Count, fill = Phase)) +
-  geom_bar(stat = 'identity') + cowplot::theme_cowplot() +
-  coord_flip() +
-  scale_fill_manual(values = col)
+  ## cluster facet
+  facet_plot <- pdata %>%
+    group_by(cluster) %>%
+    mutate(Percentage = (Count / sum(Count) * 100)) %>%
+    filter(Percentage > perc_cutoff)  %>%
+    mutate(cluster = factor(cluster, levels = seq(1,max(pdata$cluster %>% as.numeric()),1) %>% as.character())) %>%
+    ggplot(aes(x=value, y=Count, fill = value)) +
+    geom_bar(stat = 'identity') + cowplot::theme_cowplot() +
+    scale_y_continuous(expand = c(0,0)) +
+    coord_flip() +
+    scale_fill_manual(values = col) +
+    facet_wrap(~cluster, scales = 'free', ncol = 6) +
+    theme(legend.position = "none") +
+    xlab(faceting_on)
+  facet_plot
+}
 
-## organism
-pdata <- meta_filter_TM %>% ungroup()%>% group_by(organism,  cluster) %>%
-  summarise(Count = n()) %>%
-  left_join(cat_to_color_df %>%
-              filter(meta_category == 'organism') %>%
-              select(value, color) %>% unique(),
-            by = c('organism' = 'value')) %>%
-  ungroup()
-col <- pdata$color
-names(col) <- pdata$organism
-org_plot <- pdata %>%
-  mutate(cluster = factor(cluster, levels = seq(1,max(pdata$cluster %>% as.numeric()),1) %>% as.character())) %>%
-  ggplot(aes(x=cluster, y= Count, fill = organism)) +
-  geom_bar(stat = 'identity') + cowplot::theme_cowplot() +
-  coord_flip() +
-  scale_fill_manual(values = col)
+cluster_facet_maker(meta_filter_TM, 'CellType_predict')
+
+cowplot::plot_grid(
+  cluster_facet_maker(meta_filter_TM, 'CellType_predict', 1),
+  cluster_facet_maker(meta_filter_TM, 'Phase', 1),
+  cluster_facet_maker(meta_filter_TM, 'study_accession', 1, 5),
+  cluster_facet_maker(meta_filter_TM, 'organism', 1),
+  align = 'hv',
+  ncol = 1)
 
 
+cowplot::plot_grid(
+  cluster_facet_maker(meta_filter_TM, 'CellType_predict',66),
+  cluster_facet_maker(meta_filter_TM, 'Phase', 66),
+  cluster_facet_maker(meta_filter_TM, 'study_accession', 66, 5),
+  cluster_facet_maker(meta_filter_TM, 'organism', 66),
+  align = 'hv',
+  ncol = 1)
 
-
-#
-#
-# meta_filter %>% group_by(cluster, organism) %>%
-#   summarise(Count = n()) %>%
-#   mutate(Percent = (Count/sum(Count)) * 100) %>% mutate(value = case_when(Percent > 15.0 ~ organism, TRUE ~ '')) %>% ungroup() %>% group_by(cluster) %>% summarise(value = paste(value, collapse = ' ')) %>% mutate(value = gsub('\\s\\s+', ' ', value))
